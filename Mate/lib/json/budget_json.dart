1ee72c5f1ef181a62c787a41a7972ca2901d5f96
Marlon-Orrego <marlonorrego118@gmail.com>
2023-04-26 21:03:49 -0500

    Corrección Datos Json y Alcancias

---
+++ Mate/lib/json/budget_json.dart
@@ -0,0 +1,25 @@
+import 'package:bankingapp/theme/colors.dart';
+
+const List budget_json = [
+  {
+    "name": "Carro",
+    "price": "\$2250.00",
+    "label_percentage": "45%",
+    "percentage": 0.45,
+    "color": green
+  },
+  {
+    "name": "Computador",
+    "price": "\$3000.00",
+    "label_percentage": "70%",
+    "percentage": 0.7,
+    "color": red
+  },
+  {
+    "name": "Banco",
+    "price": "\$4000.00",
+    "label_percentage": "90%",
+    "percentage": 0.9,
+    "color": blue
+  }
+];
---
+++ Mate/lib/json/create_budget_json.dart
@@ -0,0 +1,8 @@
+const List categories = [
+  {"name": "Transporte", "icon": "assets/images/auto.png"},
+  {"name": "Banco", "icon": "assets/images/bank.png"},
+  {"name": "Dinero", "icon": "assets/images/cash.png"},
+  {"name": "Comida", "icon": "assets/images/charity.png"},
+  {"name": "Gimnasio", "icon": "assets/images/eating.png"},
+  {"name": "Gasolina", "icon": "assets/images/gift.png"}
+];
