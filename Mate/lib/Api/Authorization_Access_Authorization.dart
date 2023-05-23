76738a5b8de59505718a6aed43dd5a50b315b7bc
Valerias-mr <valeria.roldan1358@gmail.com>
2023-05-23 15:35:49 -0500

    Correcciones generales

--- Mate/lib/Api/Authorization_Access_Authorization.dart
+++ Mate/lib/Api/Authorization_Access_Authorization.dart
@@ -1,13 +1,12 @@
-import 'dart:math';
-
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<String> obtenerAccessToken() async {
+Future<dynamic> obtenerAccessToken() async {
   final String apiUrl =
-      'https://gw-sandbox-qa.apps.ambientesbc.com/public-partner/sb/security/oauth-provider/oauth2/token';
+      'https://gw-sandbox-qa.apps.ambientesbc.com/public-partner/sb/security/oauth-provider/oauth2/token'; // Reemplaza con la URL de tu API
 
-  Map<String, String> body = {
+  // Datos necesarios en el body del POST
+  Map<String, String> Body = {
     'grant_type': 'client_credentials',
     'scope':
         'Customer-viability:read:app Customer-token:write:user Validate-otp:read:user TermsConditions:read:user TermsConditions-register:write:user Products-payment:read:user BancolombiaPay-wallet:write:user Product-balance:read:user Payment:write:user VoidTransfer-payment:write:user SecurityCode:read:app SecurityCode:write:app Qr-transaction:write:user Qr:read:app',
@@ -15,27 +14,27 @@ Future<String> obtenerAccessToken() async {
     'client_secret': 'cda9962310cbbdd10155364b56685884',
   };
 
-  Map<String, String> headers = {
+  // Headers requeridos
+  Map<String, String> Headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept': 'application/json',
-    'Host': 'gw-sandbox-qa.apps.ambientesbc.com',
-    'X-Forwarded-Host':
-        'gw-sandbox-qa.apps.ambientesbc.com', // Agrega el valor correcto del Host
   };
 
-  String basicAuth = base64Encode(
-      utf8.encode('${body['client_id']}:${body['client_secret']}'));
-  headers['Authorization'] = 'Basic $basicAuth';
+  var response = await http.post(
+    Uri.parse(apiUrl),
+    body: Body,
+    headers: Headers,
+  );
 
-  var response =
-      await http.post(Uri.parse(apiUrl), body: body, headers: headers);
-
+  // Comprobar el estado de la respuesta
   if (response.statusCode == 200) {
+    // Decodificar la respuesta JSON
     Map<String, dynamic> data = json.decode(response.body);
     String accessToken = data['access_token'];
     return accessToken;
   } else {
+    // En caso de error, imprimir el código de estado y el mensaje de error
     print('Error ${response.statusCode}: ${response.body}');
-    return "Error";
+    return null;
   }
 }
--- Mate/lib/main.dart
+++ Mate/lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
               stream: FirebaseAuth.instance.authStateChanges(),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return BancolombiaLoginPage();
+                  return HomeScreen();
                 } else {
                   return LoginPage();
                 }
